07aa4845a1623ec47399dcc6d8365ea8d284506c
Victor Silva <victor.silva452@outlook.com>
2019-10-19 17:14:52 -0300

    Selection Sort folder updated

--- SelectionSort/selectionSort.c
+++ SelectionSort/selectionSort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int selectionSortUpdated(int arr[], int size) 
+int selection_sort_optimized(int arr[], int size)
 {  
     for (int i = 0; i < size - 1; i++)  { 
         int min = i; 
@@ -12,7 +12,7 @@ int selectionSortUpdated(int arr[], int size)
 			int key = arr[min]; 
 			while (min > i)  
 			{ 
-				arr[min] = arr[min - 1]; 
+				arr[min] = arr[min - 1];
 				min--; 
 			} 
         	arr[i] = key; 
@@ -20,7 +20,7 @@ int selectionSortUpdated(int arr[], int size)
 } 
   
 
-int selectionSort(int arr[], int size){
+int selection_sort(int arr[], int size){
 	int aux = 0;
 	for (int i = 0; i < size-1; i++) {
 		aux = i;
@@ -32,35 +32,4 @@ int selectionSort(int arr[], int size){
 			}
 		}
 	}
-}
-
-void printArray(int arr[], int n){
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d", arr[i]);
-		if (i != n-1) printf(", "); else printf("\n");
-	}
-}
-
-int main(int argc, char const *argv[])
-{
-	int arr[20];
-	int size = sizeof(arr)/sizeof(int);
-	int i = 0;
-
-	while (i < size){
-		arr[i] = rand() % 20;
-		i++;
-	}
-	
-	printf("Array desordenado: ");
-	printArray(arr, size);
-
-	selectionSort(arr, size);
-
-	printf("Array ordenado: ");
-	printArray(arr, size);
-		
-		
-	return 0;
 }
